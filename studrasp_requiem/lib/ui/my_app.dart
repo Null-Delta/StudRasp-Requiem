cb61910b4a39bf611b1e516cf7926487d976fe30
StarProxima <maks.prozorov1@yandex.ru>
2022-08-31 19:44:36 +0300

    IconTheme

--- studrasp_requiem/lib/ui/my_app.dart
+++ studrasp_requiem/lib/ui/my_app.dart
@@ -6,58 +6,34 @@ import '../styles/colors.dart';
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  final appLightColors = const AppLightColors();
+
+  final appDarkColors = const AppDarkColors();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       themeMode: ThemeMode.system,
-      darkTheme: ThemeData(
-        primarySwatch: Colors.blue,
-        extensions: [
-          const AppDarkColors(),
-          AppDefaultTextStyles(const AppDarkColors()),
-        ],
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.light().copyWith(
+        iconTheme: ThemeData.dark().iconTheme.copyWith(
+              color: appLightColors.accentPrimary,
+            ),
         extensions: [
           const AppLightColors(),
-          AppDefaultTextStyles(const AppLightColors()),
+          AppDefaultTextStyles(appLightColors),
+        ],
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        iconTheme: ThemeData.dark().iconTheme.copyWith(
+              color: appDarkColors.accentPrimary,
+            ),
+        extensions: [
+          const AppDarkColors(),
+          AppDefaultTextStyles(appDarkColors),
         ],
       ),
       home: const TimetablePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final colors = Theme.of(context).extension<AppColors>()!;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      backgroundColor: colors.backgroundPrimary!,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [],
-          ),
-        ),
-      ),
     );
   }
 }
