70708d58c2dfa825435a8ade374a5bcfffc1ea30
iamgirya <iamgirya@yandex.ru>
2022-09-01 18:19:08 +0300

    Провайдеры для редактирования пар
    Теперь можно редактировать пары

--- studrasp_requiem/lib/ui/lesson_editor_page/lesson_editor_page.dart
+++ studrasp_requiem/lib/ui/lesson_editor_page/lesson_editor_page.dart
@@ -1,85 +1,178 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../gen/assets.gen.dart';
+import '../../models/day/day_model.dart';
+import '../../models/lesson/lesson_model.dart';
 import '../../styles/colors.dart';
 import '../../styles/fonts.dart';
+import '../timetable_editor_page/timetable_editor_page.dart';
 import 'widgets/lesson_part_input.dart';
 
-class LessonEditorPage extends StatelessWidget {
-  const LessonEditorPage({Key? key}) : super(key: key);
+final currentEditingLesson = StateProvider<Lesson>((ref) {
+  return const Lesson(name: '', type: '', teacher: '', audience: '');
+});
+
+class LessonEditorPage extends ConsumerStatefulWidget {
+  const LessonEditorPage({
+    Key? key,
+    required this.lessonDay,
+    required this.lessonNumber,
+  }) : super(key: key);
+
+  final int lessonDay;
+  final int lessonNumber;
+
+  @override
+  ConsumerState<LessonEditorPage> createState() => _LessonEditorPageState();
+}
+
+class _LessonEditorPageState extends ConsumerState<LessonEditorPage> {
+  late TextEditingController nameTextEditingController = TextEditingController()
+    ..text = ref
+        .read(currentEditingTimetable)
+        .days[widget.lessonDay]
+        .lessons[widget.lessonNumber]
+        .name;
+  late TextEditingController audienceTextEditingController =
+      TextEditingController()
+        ..text = ref
+            .read(currentEditingTimetable)
+            .days[widget.lessonDay]
+            .lessons[widget.lessonNumber]
+            .audience;
+  late TextEditingController teacherTextEditingController =
+      TextEditingController()
+        ..text = ref
+            .read(currentEditingTimetable)
+            .days[widget.lessonDay]
+            .lessons[widget.lessonNumber]
+            .teacher;
+  late TextEditingController typeTextEditingController = TextEditingController()
+    ..text = ref
+        .read(currentEditingTimetable)
+        .days[widget.lessonDay]
+        .lessons[widget.lessonNumber]
+        .type;
+
+  void saveChanges() {
+    Lesson newLesson = Lesson(
+        name: nameTextEditingController.text,
+        type: typeTextEditingController.text,
+        teacher: teacherTextEditingController.text,
+        audience: audienceTextEditingController.text);
+
+    ref.read(currentEditingTimetable.notifier).update(
+      (state) {
+        List<Day> newDays = List<Day>.from(state.days)
+          ..[widget.lessonDay] = Day(
+            lessons: List<Lesson>.from(state.days[widget.lessonDay].lessons)
+              ..[widget.lessonNumber] = newLesson,
+          );
+
+        return state.copyWith(
+          days: newDays,
+        );
+      },
+    );
+
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).extension<AppColors>()!;
     final textStyles = Theme.of(context).extension<AppTextStyles>()!;
 
-    return Scaffold(
-      backgroundColor: colors.backgroundPrimary,
-      appBar: AppBar(
-        elevation: 0,
+    final lesson = ref.watch(
+      currentEditingTimetable.select(
+        (value) => value.days[widget.lessonDay].lessons[widget.lessonNumber],
+      ),
+    );
+
+    return ProviderScope(
+      overrides: [
+        currentEditingLesson.overrideWithValue(StateController(lesson)),
+      ],
+      child: Scaffold(
         backgroundColor: colors.backgroundPrimary,
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 42,
-              height: 42,
-              padding: const EdgeInsets.only(left: 16),
-              child: IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  // переход назад
-                },
-                icon: SvgPicture.asset(
-                  Assets.images.circleChevronLeft.path,
-                  color: colors.accentPrimary,
-                  fit: BoxFit.cover,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: colors.backgroundPrimary,
+          title: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 42,
+                height: 42,
+                padding: const EdgeInsets.only(left: 16),
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    // переход назад и сохранение
+                    saveChanges();
+                  },
+                  icon: SvgPicture.asset(
+                    Assets.images.circleChevronLeft.path,
+                    color: colors.accentPrimary,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+              Text(
+                'Пара',
+                style: textStyles.title,
+              ),
+              const SizedBox(
+                width: 42,
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: colors.separator,
             ),
-            Text(
-              'Пара',
-              style: textStyles.title,
-            ),
-            const SizedBox(
-              width: 42,
-              height: 42,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  LessonPartInput(
+                    partName: 'Название предмета',
+                    controller: nameTextEditingController,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  LessonPartInput(
+                    partName: 'Аудитория',
+                    controller: audienceTextEditingController,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  LessonPartInput(
+                    partName: 'Преподаватель',
+                    controller: teacherTextEditingController,
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  LessonPartInput(
+                    partName: 'Тип',
+                    controller: typeTextEditingController,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: colors.separator,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: const [
-                LessonPartInput(
-                  partName: 'Название предмета',
-                ),
-                SizedBox(
-                  height: 12,
-                ),
-                LessonPartInput(partName: 'Аудитория'),
-                SizedBox(
-                  height: 12,
-                ),
-                LessonPartInput(partName: 'Преподаватель'),
-                SizedBox(
-                  height: 12,
-                ),
-                LessonPartInput(partName: 'Тип'),
-              ],
-            ),
-          ),
-        ],
       ),
     );
   }
--- studrasp_requiem/lib/ui/timetable_editor_page/timetable_editor_page.dart
+++ studrasp_requiem/lib/ui/timetable_editor_page/timetable_editor_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../gen/assets.gen.dart';
@@ -8,146 +9,194 @@ import '../../styles/colors.dart';
 import '../../styles/fonts.dart';
 import '../lesson_card/card_styles/editable_lesson_card.dart';
 import '../lesson_card/card_styles/empty_lesson_card.dart';
+import '../lesson_editor_page/lesson_editor_page.dart';
 import '../timetable_settings_page/labeled_text.dart';
+import '../timetable_settings_page/timetable_settings_page.dart';
 import '../widgets/popup_menu_action.dart';
 import '../widgets/week_timeline.dart';
 
-class TimetableEditorPage extends StatelessWidget {
-  TimetableEditorPage({Key? key, Timetable? editingTimetable})
-      : timeTable = editingTimetable ?? Timetable.empty(),
-        super(key: key);
+final currentEditingTimetable = StateProvider<Timetable>((ref) {
+  return Timetable.empty();
+});
 
-  final Timetable timeTable;
+class TimetableEditorPage extends ConsumerWidget {
+  const TimetableEditorPage({Key? key, this.timeTable}) : super(key: key);
+
+  final Timetable? timeTable;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).extension<AppColors>()!;
     final textStyles = Theme.of(context).extension<AppTextStyles>()!;
 
-    return Scaffold(
-      backgroundColor: colors.backgroundPrimary,
-      appBar: AppBar(
-        elevation: 0,
+    final config =
+        ref.watch(currentEditingTimetable.select((value) => value.config));
+    final name =
+        ref.watch(currentEditingTimetable.select((value) => value.name));
+    final days =
+        ref.watch(currentEditingTimetable.select((value) => value.days));
+
+    final selectedDay = 0;
+
+    return ProviderScope(
+      overrides: [
+        currentEditingTimetable
+            .overrideWithValue(StateController(timeTable ?? Timetable.empty()))
+      ],
+      child: Scaffold(
         backgroundColor: colors.backgroundPrimary,
-        title: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: 42,
-              height: 42,
-              padding: const EdgeInsets.only(left: 16),
-              child: IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  // переход назад
-                },
-                icon: SvgPicture.asset(
-                  Assets.images.circleChevronLeft.path,
-                  color: colors.accentPrimary,
-                  fit: BoxFit.cover,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: colors.backgroundPrimary,
+          title: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 42,
+                height: 42,
+                padding: const EdgeInsets.only(left: 16),
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    // переход назад временно
+                    Navigator.of(context).pop();
+                  },
+                  icon: SvgPicture.asset(
+                    Assets.images.circleChevronLeft.path,
+                    color: colors.accentPrimary,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
+              Text(
+                name,
+                style: textStyles.title,
+              ),
+              Container(
+                width: 42,
+                height: 42,
+                padding: const EdgeInsets.only(right: 16),
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  onPressed: () {
+                    // временно переход в настройки
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const TimeTableSettingsPage();
+                        },
+                      ),
+                    );
+                  },
+                  icon: SvgPicture.asset(
+                    Assets.images.settings.path,
+                    color: colors.accentPrimary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Column(
+          children: [
+            const WeekTimeline(),
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: colors.separator,
             ),
-            Text(
-              timeTable.name,
-              style: textStyles.title,
+            LabeledText(label: 'Неделя', text: config.weekTypes[0]),
+            const SizedBox(
+              height: 12,
             ),
-            Container(
-              width: 42,
-              height: 42,
-              padding: const EdgeInsets.only(right: 16),
-              child: IconButton(
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  // переход назад
-                },
-                icon: SvgPicture.asset(
-                  Assets.images.settings.path,
-                  color: colors.accentPrimary,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                ),
+                child: ListView.separated(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: days[selectedDay].lessons.length,
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(
+                      height: 12,
+                    );
+                  },
+                  itemBuilder: (context, index) {
+                    if (days[selectedDay].lessons[index].isEmpty) {
+                      return EmptyLessonCard(
+                        index: index + 1,
+                        // взять из заданного времени
+                        interval: TimeInterval(
+                          from: config.timeIntervals[index].from,
+                          to: config.timeIntervals[index].to,
+                        ),
+                        onTap: () {
+                          //переход в редактор пары
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return LessonEditorPage(
+                                  lessonDay: selectedDay,
+                                  lessonNumber: index,
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return EditableLessonCard(
+                        index: index + 1,
+                        // взять из заданного времени
+                        interval: TimeInterval(
+                          from: config.timeIntervals[index].from,
+                          to: config.timeIntervals[index].to,
+                        ),
+                        lesson: days[selectedDay].lessons[index],
+                        actions: [
+                          // реализовать действия
+                          PopupMenuAction(
+                            text: "Изменить",
+                            icon: Assets.images.iconEditOutline
+                                .svg(color: colors.accentPrimary),
+                            action: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) {
+                                    return LessonEditorPage(
+                                      lessonDay: selectedDay,
+                                      lessonNumber: index,
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                          PopupMenuAction(
+                            text: "Копировать пару",
+                            icon: Assets.images.command
+                                .svg(color: colors.accentPrimary),
+                            action: () {},
+                          ),
+                          PopupMenuAction(
+                            text: "Удалить",
+                            icon: Assets.images.trashFull
+                                .svg(color: colors.accentPrimary),
+                            action: () {},
+                            style: PopupMenuActionStyle.destructive,
+                          ),
+                        ],
+                      );
+                    }
+                  },
                 ),
               ),
             ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          const WeekTimeline(),
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: colors.separator,
-          ),
-          LabeledText(label: 'Неделя', text: timeTable.config.weekTypes[0]),
-          const SizedBox(
-            height: 12,
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                left: 16,
-                right: 16,
-              ),
-              child: ListView.separated(
-                physics: const BouncingScrollPhysics(),
-                itemCount: timeTable.days[0].lessons.length,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(
-                    height: 12,
-                  );
-                },
-                itemBuilder: (context, index) {
-                  if (timeTable.days[0].lessons[index].isEmpty) {
-                    return EmptyLessonCard(
-                      index: index + 1,
-                      // взять из заданного времени
-                      interval: TimeInterval(
-                        from: timeTable.config.timeIntervals[index].from,
-                        to: timeTable.config.timeIntervals[index].to,
-                      ),
-                      onTap: () {
-                        //переход в редактор пары
-                      },
-                    );
-                  } else {
-                    return EditableLessonCard(
-                      index: index + 1,
-                      // взять из заданного времени
-                      interval: TimeInterval(
-                        from: timeTable.config.timeIntervals[index].from,
-                        to: timeTable.config.timeIntervals[index].to,
-                      ),
-                      lesson: timeTable.days[0].lessons[index],
-                      actions: [
-                        // реализовать действия
-                        PopupMenuAction(
-                          text: "Изменить",
-                          icon: Assets.images.iconEditOutline
-                              .svg(color: colors.accentPrimary),
-                          action: () {},
-                        ),
-                        PopupMenuAction(
-                          text: "Копировать пару",
-                          icon: Assets.images.command
-                              .svg(color: colors.accentPrimary),
-                          action: () {},
-                        ),
-                        PopupMenuAction(
-                          text: "Удалить",
-                          icon: Assets.images.trashFull
-                              .svg(color: colors.accentPrimary),
-                          action: () {},
-                          style: PopupMenuActionStyle.destructive,
-                        ),
-                      ],
-                    );
-                  }
-                },
-              ),
-            ),
-          ),
-        ],
       ),
     );
   }
