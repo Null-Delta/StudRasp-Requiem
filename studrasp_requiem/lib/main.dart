911bafd2ad0ec64bc39e60109567b5b4bd4a3672
StarProxima <maks.prozorov1@yandex.ru>
2022-09-02 23:59:05 +0300

    Add Firebase in Project

--- studrasp_requiem/lib/main.dart
+++ studrasp_requiem/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'support/logger.dart';
@@ -7,15 +8,22 @@ import 'support/logger.dart';
 import 'support/error_handler.dart';
 import 'ui/my_app.dart';
 
+import 'firebase_options.dart';
+
 void main() {
   runZonedGuarded(
     () async {
       // await initFirebase();
+      WidgetsFlutterBinding.ensureInitialized();
 
       initLogger();
       logger.info('Start main');
 
       ErrorHandler.init();
+
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
 
       runApp(
         const ProviderScope(
