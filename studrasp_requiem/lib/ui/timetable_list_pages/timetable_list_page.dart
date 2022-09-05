4edba914e7332b2d5273a50c48275d36d91af6ea
Delta Null <delta.null@vk.com>
2022-09-05 14:12:43 +0300

    Change TimeTableListPage

--- studrasp_requiem/lib/ui/timetable_list_pages/timetable_list_page.dart
+++ studrasp_requiem/lib/ui/timetable_list_pages/timetable_list_page.dart
@@ -18,10 +18,11 @@ class TimetableListPage extends ConsumerStatefulWidget {
 
 class _TimetableListPageState extends ConsumerState<TimetableListPage> {
   ScrollController savedTimeTablesContoller = ScrollController();
-  ScrollController myTimeTablesContoller = ScrollController();
 
   List<Timetable> savedTables = [];
   List<Timetable> myTables = [];
+
+  bool showDivider = false;
 
   @override
   void initState() {
@@ -38,10 +39,24 @@ class _TimetableListPageState extends ConsumerState<TimetableListPage> {
     });
   }
 
+  void onScroll() {
+    if (savedTimeTablesContoller.offset > 0 && !showDivider) {
+      setState(() {
+        showDivider = true;
+      });
+    } else if (savedTimeTablesContoller.offset <= 0 && showDivider) {
+      setState(() {
+        showDivider = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final colors = context.colors;
     final textStyles = context.textStyles;
+
+    savedTimeTablesContoller.addListener(onScroll);
 
     return DefaultTabController(
       length: 2,
@@ -66,154 +81,84 @@ class _TimetableListPageState extends ConsumerState<TimetableListPage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
               child: Text(
-                "Список\nрасписаний",
+                "Сохраненные\nрасписания",
                 style: textStyles.largeTitle!,
               ),
             ),
-            const SizedBox(
-              height: 12,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: colors.backgroundSecondary,
-                  borderRadius: BorderRadiusStyles.large,
-                  border: Border.all(color: colors.separator!, width: 1),
-                ),
-                padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 4),
-                child: TabBar(
-                  labelColor: colors.backgroundPrimary,
-                  labelStyle: textStyles.label!,
-                  unselectedLabelStyle: textStyles.label!.copyWith(color: colors.accentPrimary),
-                  unselectedLabelColor: colors.accentPrimary,
-                  indicatorWeight: 0,
-                  indicator: BoxDecoration(
-                    color: colors.accentPrimary,
-                    borderRadius: BorderRadiusStyles.normal,
-                  ),
-                  tabs: [
-                    Container(
-                      alignment: Alignment.center,
-                      height: 36,
-                      child: const Text("Сохраненные"),
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      height: 36,
-                      child: const Text("Мои"),
-                    ),
-                  ],
-                ),
-              ),
-            ),
             const SizedBox(height: 16),
-            Divider(
-              height: 1,
-              thickness: 1,
-              color: colors.separator,
-            ),
+            if (showDivider)
+              Divider(
+                height: 1,
+                thickness: 1,
+                color: colors.separator,
+              ),
             Expanded(
-              child: TabBarView(
-                children: [
-                  RefreshIndicator(
-                    onRefresh: () async {
-                      await loadSavedTables(ref.read(localStorage).savedTimetables);
-                    },
-                    child: savedTables.isEmpty
-                        ? ListView(
-                            children: [
-                              Container(
-                                alignment: Alignment.center,
-                                height: 128,
-                                child: Text(
-                                  "Список пуст",
-                                  style: textStyles.label!.copyWith(color: colors.disable),
-                                ),
-                              ),
-                            ],
-                          )
-                        : ListView.separated(
-                            physics: const BouncingScrollPhysics(
-                              parent: AlwaysScrollableScrollPhysics(),
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  await loadSavedTables(ref.read(localStorage).savedTimetables);
+                },
+                child: savedTables.isEmpty
+                    ? ListView(
+                        children: [
+                          Container(
+                            alignment: Alignment.center,
+                            height: 96,
+                            child: Text(
+                              "Список пуст",
+                              style: textStyles.label!.copyWith(color: colors.disable),
                             ),
-                            controller: savedTimeTablesContoller,
-                            itemBuilder: (context, index) {
-                              return TimetableCard(
-                                timeTable: savedTables[index],
-                                button: PopupMenuButton(
-                                  iconSize: 24,
-                                  icon: Assets.images.moreHorizontal.svg(color: colors.accentPrimary),
-                                  itemBuilder: (context) {
-                                    return [
-                                      PopupMenuItem(
-                                        child: const Text("Использовать"),
-                                        onTap: () {
-                                          ref.read(localStorage.notifier).save(savedTables[index]);
-                                          Navigator.pop(context);
-                                        },
-                                      ),
-                                      PopupMenuItem(
-                                        value: 1,
-                                        child: const Text("Удалить"),
-                                        onTap: () {
-                                          final id = savedTables[index].id;
-                                          ref.read(localStorage.notifier).removeFromSavedTimeTables(id);
-                                          setState(() {
-                                            savedTables.removeWhere((table) => table.id == id);
-                                          });
-                                        },
-                                      ),
-                                    ];
-                                  },
-                                ),
-                                onTap: () {
-                                  ref.read(localStorage.notifier).save(savedTables[index]);
-                                  Navigator.pop(context);
-                                },
-                              );
-                            },
-                            separatorBuilder: (context, index) {
-                              return Divider(
-                                height: 1,
-                                thickness: 1,
-                                color: colors.separator,
-                              );
-                            },
-                            itemCount: savedTables.length,
                           ),
-                  ),
-                  ListView.separated(
-                    physics: const BouncingScrollPhysics(
-                      parent: AlwaysScrollableScrollPhysics(),
-                    ),
-                    controller: myTimeTablesContoller,
-                    itemBuilder: (context, index) {
-                      return TimetableCard(
-                        timeTable: myTables[index],
-                        button: Assets.images.moreHorizontal.svg(color: colors.accentPrimary),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const TimetableEditorPage();
+                        ],
+                      )
+                    : ListView.separated(
+                        physics: const BouncingScrollPhysics(
+                          parent: AlwaysScrollableScrollPhysics(),
+                        ),
+                        controller: savedTimeTablesContoller,
+                        itemBuilder: (context, index) {
+                          return TimetableCard(
+                            timeTable: savedTables[index],
+                            button: PopupMenuButton(
+                              iconSize: 24,
+                              icon: Assets.images.moreHorizontal.svg(color: colors.accentPrimary),
+                              itemBuilder: (context) {
+                                return [
+                                  PopupMenuItem(
+                                    child: const Text("Использовать"),
+                                    onTap: () {
+                                      ref.read(localStorage.notifier).save(savedTables[index]);
+                                      Navigator.pop(context);
+                                    },
+                                  ),
+                                  PopupMenuItem(
+                                    value: 1,
+                                    child: const Text("Удалить"),
+                                    onTap: () {
+                                      final id = savedTables[index].id;
+                                      ref.read(localStorage.notifier).removeFromSavedTimeTables(id);
+                                      setState(() {
+                                        savedTables.removeWhere((table) => table.id == id);
+                                      });
+                                    },
+                                  ),
+                                ];
                               },
                             ),
+                            onTap: () {
+                              ref.read(localStorage.notifier).save(savedTables[index]);
+                              Navigator.pop(context);
+                            },
                           );
                         },
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return Divider(
-                        height: 1,
-                        thickness: 1,
-                        color: colors.separator,
-                      );
-                    },
-                    itemCount: myTables.length,
-                  ),
-                ],
+                        separatorBuilder: (context, index) {
+                          return Divider(
+                            height: 1,
+                            thickness: 1,
+                            color: colors.separator,
+                          );
+                        },
+                        itemCount: savedTables.length,
+                      ),
               ),
             ),
           ],
